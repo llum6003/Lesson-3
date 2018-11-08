3a0175ed632c0ff046a8839e2f83abc55c56db18
llum6003 <44827939+llum6003@users.noreply.github.com>
2018-11-08 01:26:52 -0500

    Add files via upload

---
+++ FirstJavaProgram/src/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstjavaprogram;
+
+/**
+ *
+ * @author leonardolumpford
+ */
+public class FirstJavaProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            System.out.println("This is my first program in java");
+    }//End of main
+    
+}//End of FirstJavaProgram Class
